ba7d0cd3d3fd731cb958438d5d1bfd01f3a84602
Administrator <admin@nstrans.by>
2018-07-20 09:58:06 +0300

    First commit

--- find_dups.sh
+++ find_dups.sh
@@ -8,9 +8,9 @@ if [ -z "$2" ]
 then
     echo "usage: `basename $0` [directory] [method] [scriptname] \n"
     echo "directory - where to search files"
-    echo "method :\n \\t name - search files have same name \n \\t cont - files have same content" 
+    echo "method :\n \\t name - search files having same name \n \\t cont - files having same content" 
     echo "scriptname - name of generatable removal script ('rm_dups.sh' by default)"
-    exit 65
+    exit 0
 else
     search_dir=$1
     if [ -n "$3" ]; then
@@ -19,17 +19,17 @@ else
 fi
 
 case $2 in
-name) echo "#!/bin/sh \n\n##### List of files have same name ##### \n" > $rm_script
+name) echo "#!/bin/sh \n##### List of files having same name ##### \n" > $rm_script
       #find $search_dir -type f -printf "%f\n" | sort | uniq --all-repeated=separate -D | sed -e "s/^/#rm $search_dir\//" >> $rm_script ;;
       find $search_dir -type f  > $tempfile
-      cat $tempfile | sed 's_.*/__' | sort |  uniq -d| 
+      cat $tempfile | sed 's_.*/__' | sort |  uniq -d | 
       while read fileName
       do
 	  grep "$fileName" $tempfile | sed "s/^/#rm /" | xargs -0 echo  >> $rm_script
       done
-      #> /dev/null
       rm $tempfile;;
-cont) echo "#!/bin/sh \n\n ##### List of files have same content ##### \n" > $rm_script
+      
+cont) echo "#!/bin/sh \n\n ##### List of files having same content ##### \n" > $rm_script
       find $search_dir -type f -exec md5sum '{}' ';' \
       | sort | uniq --all-repeated=separate -w 33 | sed "s/.\{34\}/#rm /" >> $rm_script ;;
 esac
---
+++ rm_dups.sh
@@ -0,0 +1,50 @@
+#!/bin/sh 
+
+##### List of files have same name ##### 
+
+#rm ./4/1'.txt
+#rm ./7/1'.txt
+
+#rm ./1.txt/2.txt
+#rm ./2 1/2.txt
+#rm ./2/2.txt
+
+#rm ./6/3.txt
+#rm ./1.txt/3.txt
+#rm ./2 1/3.txt
+#rm ./3/3.txt
+#rm ./2/3.txt
+
+#rm ./1.txt/4.txt
+#rm ./3/4.txt
+
+#rm ./4/ 50.txt
+#rm ./5/ 50.txt
+
+#rm ./6/5.txt
+#rm ./1.txt/5.txt
+#rm ./3/5.txt
+
+#rm ./1.txt/6.txt
+#rm ./5/6.txt
+
+#rm ./1.txt/7.txt
+#rm ./7/7.txt
+
+#rm ./1.txt/8.txt
+#rm ./7/8.txt
+
+#rm ./4/9 1.txt
+#rm ./7/9 1.txt
+
+#rm ./1.txt/9.txt
+#rm ./7/9.txt
+
+#rm ./.git/logs/HEAD
+#rm ./.git/HEAD
+
+#rm ./.git/logs/refs/remotes/origin/master
+#rm ./.git/logs/refs/heads/master
+#rm ./.git/refs/remotes/origin/master
+#rm ./.git/refs/heads/master
+
